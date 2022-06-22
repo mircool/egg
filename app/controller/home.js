3ccbf709e202497184b832efdf6ccac85e889dd7
mircool <ynyjyz@163.com>
2022-06-23 07:12:58 +0800

    Initial commit

---
+++ app/controller/home.js
@@ -0,0 +1,10 @@
+const Controller = require('egg').Controller
+
+class HomeController extends Controller {
+    async index() {
+        this.ctx.body = 'hello egg'
+    }
+
+}
+
+module.exports = HomeController
