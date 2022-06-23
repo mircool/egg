a2a15e636f52ad9d3e67d8ce9347749a1178d83a
mircool <ynyjyz@163.com>
2022-06-23 11:04:37 +0800

    小试身手

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,9 +2,11 @@ const Controller = require('egg').Controller
 
 class HomeController extends Controller {
     async index() {
-        this.ctx.body = 'hello egg'
-    }
+        // this.ctx.body = 'hello egg'
+        await this.ctx.render('index.html',{message:'egg'})
 
+
+    }
 }
 
-module.exports = HomeController
+module.exports = HomeController;
--- config/config.default.js
+++ config/config.default.js
@@ -1,1 +1,8 @@
 exports.keys = 'aaaaaaaaaaaaaaaaaa'
+
+exports.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+        '.html': 'nunjucks',
+    },
+};
